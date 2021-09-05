0d78cfe4ba792897644b5596a23e9152979d747c
Abhishek Bose <abose550@gmail.com>
2021-09-05 09:52:56 +0530

    added main.py inside app

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using cookiecutter template to test out the data science template',
+    author='Abhishek Bose',
+    license='MIT',
+)
